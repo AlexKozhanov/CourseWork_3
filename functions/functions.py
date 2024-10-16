f9c3726d962ee35afd2660021a5c6a8b28498b3a
Admin <alexander_kozhanov@mail.ru>
2024-10-16 21:29:22 +0300

    New branch DEVELOP

--- functions/functions.py
+++ functions/functions.py
@@ -1,6 +1,16 @@
-def main():
-    pass
+import json
+import os
 
+def get_json_file() -> list[dict, ...]:
+    """
+    Функция считывает файл Json
+    :return: список словарей
+    """
 
-if __name__ == '__main__':
-    main()
+    current_file_path = os.path.abspath(__file__)
+    project_root = os.path.dirname(os.path.dirname(current_file_path))
+    relative_path = os.path.join(project_root, 'operations', 'operations.json')
+
+    with open(relative_path, 'r', encoding='utf-8') as file:
+        data = json.load(file)
+        return data
